f73fd8d56ee3145899915af45a546ea682cfc9f0
ahelsayed <ahmed.m.elsayed93@gmail.com>
2018-04-04 13:17:35 +0200

    refactor judger

---
+++ tools/exit_codes.py
@@ -0,0 +1,142 @@
+exit_codes_table = {
+    1: {
+		"name": "SIGHUP",
+		"descr": "Hangup"
+	},
+	2: {
+		"name": "SIGINT",
+		"descr": "Interrupt"
+	},
+	3: {
+		"name": "SIGQUIT",
+		"descr": "Quit"
+	},
+	4: {
+		"name": "SIGILL",
+		"descr": "Illegal"
+	},
+	5: {
+		"name": "SIGTRAP",
+		"descr": "Trace/Breakpoint"
+	},
+	6: {
+		"name": "SIGABRT",
+		"descr": "Abort"
+	},
+	7: {
+		"name": "SIGEMT",
+		"descr": "Emulation"
+	},
+	8: {
+		"name": "SIGFPE",
+		"descr": "Arithmetic"
+	},
+	9: {
+		"name": "SIGKILL",
+		"descr": "Killed"
+	},
+	10: {
+		"name": "SIGBUS",
+		"descr": "Bus"
+	},
+	11: {
+		"name": "SIGSEGV",
+		"descr": "Segmentation"
+	},
+	12: {
+		"name": "SIGSYS",
+		"descr": "Bad"
+	},
+	13: {
+		"name": "SIGPIPE",
+		"descr": "Broken"
+	},
+	14: {
+		"name": "SIGALRM",
+		"descr": "Alarm"
+	},
+	15: {
+		"name": "SIGTERM",
+		"descr": "Terminated"
+	},
+	16: {
+		"name": "SIGUSR1",
+		"descr": "User"
+	},
+	17: {
+		"name": "SIGUSR2",
+		"descr": "User"
+	},
+	18: {
+		"name": "SIGCHLD",
+		"descr": "Child"
+	},
+	19: {
+		"name": "SIGPWR",
+		"descr": "Power"
+	},
+	20: {
+		"name": "SIGWINCH",
+		"descr": "Window"
+	},
+	21: {
+		"name": "SIGURG",
+		"descr": "Urgent"
+	},
+	22: {
+		"name": "SIGPOLL",
+		"descr": "Socket"
+	},
+	23: {
+		"name": "SIGSTOP",
+		"descr": "Stopped"
+	},
+	24: {
+		"name": "SIGTSTP",
+		"descr": "Stopped"
+	},
+	25: {
+		"name": "SIGCONT",
+		"descr": "Continued"
+	},
+	26: {
+		"name": "SIGTTIN",
+		"descr": "Stopped"
+	},
+	27: {
+		"name": "SIGTTOU",
+		"descr": "Stopped"
+	},
+	28: {
+		"name": "SIGVTALRM",
+		"descr": "Virtual"
+	},
+	29: {
+		"name": "SIGPROF",
+		"descr": "Profiling"
+	},
+	30: {
+		"name": "SIGXCPU",
+		"descr": "CPU"
+	},
+	31: {
+		"name": "SIGXFSZ",
+		"descr": "File"
+	},
+	32: {
+		"name": "SIGWAITING",
+		"descr": "All"
+	},
+	33: {
+		"name": "SIGLWP",
+		"descr": "Virtual"
+	},
+	34: {
+		"name": "SIGAIO",
+		"descr": "Asynchronous"
+	},
+	124: {
+		"name": "TIMEOUT",
+		"descr": "timeout"
+	}
+}
--- tools/judger.py
+++ tools/judger.py
@@ -1,5 +1,6 @@
 import json
 from tools.sandbox import Sandbox
+from tools.exit_codes import exit_codes_table
 
 class Judger:
     def __init__(self, reference_id):
@@ -18,24 +19,35 @@ class Judger:
         cmd = "bash -c 'g++ -w -fmessage-length=10000 -fno-diagnostics-show-caret -fdiagnostics-color=never -std=c++98 *.cpp -o output.out'"
         compiler_result = self._sandbox.execute(self.container.id, cmd)
         self.result['compiler'] = compiler_result
+
+    def __run_test(self, stdin, timeout=3):
+        cmd = 'timeout {}s ./output.out {}'.format(timeout, stdin)    
+        return self._sandbox.execute(self.container.id, cmd)
         
     def runTests(self, testcases):
         passed_tests = failed_tests = errored_tests = 0
 
         for testcase in testcases:
+            expected_stdout = bytes(testcase['expected_stdout'], encoding='ascii')
+            expected_stdout = expected_stdout.decode('unicode-escape')
+
             try:
-                testcase['expected_stdout'] = bytes(testcase['expected_stdout'], encoding='ascii').decode('unicode-escape')
-                cmd = './output.out {}'.format(testcase['stdin'])    
-                output = self._sandbox.execute(self.container.id, cmd)
+                output = self.__run_test(testcase['stdin'])
+                error_code = int(output['returncode'])
 
-                if output['returncode']:
-                    testcase['returncode'] = output['returncode']
-                    testcase['stderr'] = output['output'],                   
-                    testcase['stdout'] = None
-                    testcase['status'] = 'Failed'
-                    testcase['expected_stdout'] = repr(testcase['expected_stdout'])
-                    failed_tests += 1
+                if error_code:
+                    status = 'Errored'
+                    errored_tests += 1
 
+                    if error_code > 128:
+                        error_code = int(error_code) - 128
+
+                    error_code = exit_codes_table.get(error_code)
+
+                    if error_code:    
+                        testcase['stderr'] = "{} - {}".format(error_code['name'], error_code['descr'])  
+                    else:
+                        testcase['stderr'] = 'Unknown error'                                          
                 else:
                     if testcase['expected_stdout'].strip() == output['output'].strip():
                         status = 'Passed'
@@ -43,19 +55,20 @@ class Judger:
                     else:
                         status = 'Failed'
                         failed_tests += 1
-                
-                    testcase['returncode'] = output['returncode']
-                    testcase['stdout'] = repr(output['output'])
-                    testcase['expected_stdout'] = repr(testcase['expected_stdout'])
-                    testcase['stderr'] = None                    
-                    testcase['status'] = status
 
+                    testcase['stderr'] = None
+                
+                testcase['stdout'] = repr(output['output'])
+                testcase['status'] = status
+                testcase['returncode'] = output['returncode']
+                testcase['expected_stdout'] = repr(expected_stdout)
+                
             except:
                 testcase['returncode'] = 'N/A'
                 testcase['stdout'] = 'N/A'
-                testcase['expected_stdout'] = repr(testcase['expected_stdout'])
-                testcase['stderr'] = 'Unexpected error'
+                testcase['stderr'] = 'Unexpected Server Error'
                 testcase['status'] = "Errored"
+                testcase['expected_stdout'] = repr(expected_stdout)
 
             self.result['testcases'].append(testcase)
 
@@ -65,7 +78,6 @@ class Judger:
             'errored': errored_tests,
             'all':len(testcases)
         }
-
 
     def judge(self, testcases):
         self._setup()
