6b592572c3ce4df0989aaab850198761875a2b1a
ahelsayed <ahmed.m.elsayed93@gmail.com>
2018-05-14 22:48:03 +0200

    remove semantic files

---
+++ semantic.sh
@@ -0,0 +1,3 @@
+curl -o /tmp/semantic.zip https://codeload.github.com/Semantic-Org/Semantic-UI-CSS/zip/master
+unzip /tmp/semantic.zip -d /tmp/semantic
+mv /tmp/semantic/Semantic-UI-CSS-master ./static/semantic
