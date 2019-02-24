dc69e8ed9d070ec49d7b5d40f706ea22d343d422
ahelsayed <ahmed.m.elsayed93@gmail.com>
2019-02-24 17:56:27 +0200

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,11 @@ export SESSION_SECRET=$(openssl rand -hex 24)
 export MINIO_ACCESS_KEY=$(openssl rand -hex 24)
 export MINIO_SECRET_KEY=$(openssl rand -hex 24)
 
+echo "[*] Prepare ..."
+mkdir -p /root/.caddy
+mkdir -p /data/minio/storage
+mkdir -p /data/minio/config
+
 echo "[*] Deploying apps ..."
 docker-compose config --quiet
 docker-compose up -d
